0b780d46b7132285c248989e8da272f1e2aa9351
Addie Beseda <abeseda@gilt.com>
2011-07-16 17:40:32 -0400

    From hg to git

---
+++ src/test/scala/org/functionalkoans/forscala/PathToEnlightenment.scala
@@ -0,0 +1,39 @@
+package org.functionalkoans.forscala
+
+import org.scalatest.Suite
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class PathToEnlightenment extends Suite {
+
+
+  override def nestedSuites = List(new AboutAsserts,
+    new AboutValAndVar,
+    new AboutConstructors,
+    new AboutTuples,
+    new AboutLists,
+    new AboutMaps,
+    new AboutSets,
+    new AboutMutableMaps,
+    new AboutMutableSets,
+    new AboutPatternMatching,
+    new AboutCaseClasses,
+    new AboutOptions,
+    new AboutSequencesAndArrays,
+    new AboutNamedAndDefaultArguments,
+    new AboutForExpressions,
+    new AboutEmptyValues,
+    new AboutParentClasses,
+    new AboutAccessModifiers,
+    new AboutTypeSignatures,
+    new AboutTraits,
+    new AboutImportsAndPackages,
+    new AboutPreconditions,
+    new AboutHigherOrderFunctions,
+    new AboutUniformAccessPrinciple
+
+  )
+
+
+}
